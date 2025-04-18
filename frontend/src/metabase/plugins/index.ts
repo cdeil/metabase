617014d7c79d55a1458bdf3b4532ae2322896e00
Phoomparin Mano <phoomparin@gmail.com>
2025-04-18 21:25:14 +0700

    gate the public reducer plugins via an EE plugin

--- frontend/src/metabase/plugins/index.ts
+++ frontend/src/metabase/plugins/index.ts
@@ -554,6 +554,7 @@ export const PLUGIN_EMBEDDING_IFRAME_SDK = {
   isInteractiveEmbeddingEnabled: (_state: State) => false,
   InteractiveEmbedRoute: (_props: SdkInteractiveEmbedRouteProps): ReactNode =>
     null,
+  additionalPublicReducerPlugins: {},
 };
 
 export const PLUGIN_CONTENT_VERIFICATION = {
